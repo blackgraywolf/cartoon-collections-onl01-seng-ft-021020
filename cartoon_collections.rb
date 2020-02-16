24dbe12afd60c729e34a6989b0b079c07a53366b
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-16 01:51:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,38 +1,21 @@
- dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+ def roll_call_dwarves(array)
+   array.each_with_index do |name, index|
+     puts "#{index+1}. #{name}"
+   end 
+ end 
 
-# def roll_call_dwarves(array)
-#   array.each_with_index do |name, index|
-#     puts "#{index+1}. #{name}"
-#   end 
-# end 
+ def summon_captain_planet(array)
+   array.collect do |word| 
+     word.capitalize + "!"
+   end 
+ end 
 
-# roll_call_dwarves(dwarves)
 
-# planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-
-# def summon_captain_planet(array)
-#   array.collect do |word| 
-#     word.capitalize + "!"
-#   end 
-# end 
-
-# summon_captain_planet(planeteer_calls)
-
-# short_words = ["puff", "go", "two"]
-# assorted_words = ["two", "go", "industrious", "bop"]
-
-# def long_planeteer_calls(array)
-#   array.any? do |word| 
-#     word.length > 4 
-#   end 
-# end 
-
-# long_planeteer_calls(short_words)
-# #long_planeteer_calls(assorted_words)
-
-snacks = ["crackers", "gouda", "thyme"]
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-ingredients = ["garlic", "bread", "rosemary"]
+ def long_planeteer_calls(array)
+   array.any? do |word| 
+     word.length > 4 
+   end 
+end 
 
 def find_the_cheese(array)
   array.find do |cheese| 
@@ -40,4 +23,3 @@ def find_the_cheese(array)
     end 
   end 
   
-find_the_cheese(ingredients)
